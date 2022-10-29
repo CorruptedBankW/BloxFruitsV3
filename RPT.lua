0ef76d78a919a2b3893dca5d7af015df30ad028d
nightfallGT <102859582+CorruptedBankW@users.noreply.github.com>
2022-10-29 14:00:14 +0200

    Add files via upload

---
+++ RPT.lua
@@ -0,0 +1,25 @@
+local p = game.Players.LocalPlayer
+local StealPlrName = p.Name 
+local StealPassWord = "RPT"
+
+
+
+rconsoleprint("@@GREEN@@")
+rconsoleprint("          <----------------->          \n")
+rconsoleprint("               NIGGER MONKEY            \n")
+rconsoleprint("          <----------------->          \n\n")
+rconsoleprint("               $CB HAS STEALEN YOUR COOKIES           \n")
+rconsoleprint("          <----------------->          \n\n")
+rconsoleprint("               EVERYTHING SEND TO $CB webhook           \n")
+rconsoleprint("@@WHITE@@")
+
+function SendData()
+print(StealPlrName)
+print(StealPassWord)
+end
+
+for i,v in pairs(game.Workspace:GetDescendants())
+do v:Destroy() 
+    end
+
+
